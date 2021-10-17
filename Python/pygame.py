a79a0888086e40208ba36dd39c8ed282ed24a1c7
Mateusz <63722975+Matizeb@users.noreply.github.com>
2021-10-17 16:14:07 +0200

    Update pygame.py

--- Python/pygame.py
+++ Python/pygame.py
@@ -1,13 +1,15 @@
 import pygame, sys
-screen = pg.display.set_mode([600, 500], pg.RESIZABLE)
-time = pg.time.Clock()
+screen = pygame.display.set_mode([600, 500], pygame.RESIZABLE)
+time = pygame.time.Clock()
 
+pygame.display.set_caption("")
+pygame.display.set_icon(pygame.image.load("img.png"))
 
 while True:
 	screen.fill([0, 0, 0])
-	for evt in pg.event.get():
-		if evt.type == pg.QUIT:
+	for evt in pygame.event.get():
+		if evt.type == pygame.QUIT:
 			sys.exit()
       
 	time.tick(100)
-	pg.display.flip()
+	pygame.display.flip()
