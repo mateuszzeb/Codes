25a5bc29ac6e21d772235058026473396d494b49
Mateusz <63722975+mateuszzeb@users.noreply.github.com>
2021-12-09 20:25:55 +0100

    Delete bubble_sort.py

--- bubble_sort.py
+++
@@ -1,8 +0,0 @@
-def sort(list):
-    for j in range(1, len(list), 1):
-        for i in range(len(list)-j):
-            if list[i] > list[i+1]:
-                temp = list[i]
-                list[i] = list[i+1]
-                list[i+1] = temp
-    return list
