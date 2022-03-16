5b1837fd2bcdf9a107605d248b5fc6590e138961
Mateusz <63722975+mateuszzeb@users.noreply.github.com>
2022-03-16 17:06:25 +0100

    Update cdroom.py

--- Python/cdroom.py
+++ Python/cdroom.py
@@ -1,5 +1,7 @@
+import time, ctypes
+
 for i in range(10):
-    print('Opening..')
+    time.sleep(1)
     exec('ctypes.windll.WINMM.mciSendStringW(u"open L: type CDAudio alias L_drive", None, 0, None); ctypes.windll.WINMM.mciSendStringW(u"set L_drive door open", None, 0, None')
-    print('Closing..')
+    time.sleep(1)
     exec('ctypes.windll.WINMM.mciSendStringW(u"open L: type CDAudio alias L_drive", None, 0, None); ctypes.windll.WINMM.mciSendStringW(u"set L_drive door closed", None, 0, None)')
