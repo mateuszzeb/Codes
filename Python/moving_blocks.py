543f443971122cff9cc606bb98b5e6b5c7afccc3
Mateusz <63722975+mateuszzeb@users.noreply.github.com>
2022-06-16 21:03:46 +0200

    Update moving_blocks.py

--- Python/moving_blocks.py
+++ Python/moving_blocks.py
@@ -1,9 +1,27 @@
-import pygame, random
+# file settings.json should have at least {}
+# when you are clicking n than is creating new block on mouse position
+# but when you are clicking DEL and your mouse is over some block than that block will be deleted
+
+import pygame, random, json
 
 screen = pygame.display.set_mode([0, 0])
 
 class Block:
     objects = []
+
+    @staticmethod
+    def get_json():
+        json_data = "{"
+        i = 0
+        for obj in Block.objects:
+            json_data += f'\n\t"{i}":' + "{\n\t\t"
+            json_data += f'"x": {obj.x},\n\t\t"y": {obj.y},\n\t\t"w": {obj.width},\n\t\t"h": {obj.height},\n\t\t"c": {list(obj.color)}'
+            json_data += "\n\t},"
+            i += 1
+        json_data = json_data[:-1]
+        json_data += "\n}"
+        return json_data
+
     def create_surface(self):
         self.surface = pygame.Surface([self.width, self.height])
         self.surface.fill(self.color)
@@ -14,7 +32,7 @@ class Block:
         self.drag_y = y - self.y
         Block.objects.remove(self)
         Block.objects.append(self)
-        
+
     def __init__(self, w, h, x, y, c):
         self.width = w
         self.height = h
@@ -24,26 +42,50 @@ class Block:
         self.color = c
         Block.objects.append(self)
         self.create_surface()
-    
+
     def draw(self, surface):
         surface.blit(self.surface, [self.x, self.y])
-    
+        pygame.draw.rect(surface, [255, 255, 255], [self.x, self.y, self.width, self.height], 5)
+
     def delete(self):
         Block.objects.remove(self)
-        
-for i in range(10):
-    Block(100, 100, random.randint(0,screen.get_width()-40), random.randint(0,screen.get_height()-40), (random.randint(0,255), random.randint(0,255), random.randint(0,255)))
 
+with open("settings.json", "r") as file:
+    json_data = json.load(file)
+    for obj in json_data:
+        w = json_data[obj]['w']
+        h = json_data[obj]['h']
+        x = json_data[obj]['x']
+        y = json_data[obj]['y']
+        c = json_data[obj]['c']
+        Block(w, h, x, y, c)
+
+def exit_and_save():
+    json_data = Block.get_json()
+    with open("settings.json", "w") as file:
+        file.write(json_data)
+    exit()
 
 mouse_down = [0, 0]
 while True:
-    screen.fill([255, 255, 255])
+    screen.fill([0, 0, 0])
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
-            exit()
+            exit_and_save()
         if event.type == pygame.KEYDOWN:
             if event.key == pygame.K_ESCAPE:
-                exit()
+                exit_and_save()
+            if event.key == pygame.K_n:
+                Block(100, 100, pygame.mouse.get_pos()[0] - 50, pygame.mouse.get_pos()[1] - 50, [random.randint(0, 255), random.randint(0, 255), random.randint(0, 255)])
+            if event.key == pygame.K_DELETE:
+                deleted_block = None
+                for block in Block.objects:
+                    x, y = pygame.mouse.get_pos()
+                    if block.x <= x and block.x + block.width >= x and block.y <= y and block.y + block.height >= y:
+                        deleted_block = block
+                if deleted_block is not None:
+                    deleted_block.delete()
+
         if event.type == pygame.MOUSEBUTTONDOWN:
             for block in Block.objects:
                 x, y = pygame.mouse.get_pos()
@@ -59,13 +101,13 @@ while True:
             x, y = pygame.mouse.get_pos()
             for block in Block.objects:
                 if block.moving:
-                    block.x = x-block.drag_x
-                    block.y = y-block.drag_y
+                    block.x = x - block.drag_x
+                    block.y = y - block.drag_y
         if event.type == pygame.MOUSEBUTTONUP:
             for block in Block.objects:
                 block.moving = False
 
     for block in Block.objects:
         block.draw(screen)
-    
+
     pygame.display.flip()
